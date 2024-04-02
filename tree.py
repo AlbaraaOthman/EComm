6f0d3f6575af2124e113fc66cb585f923068649e
AlbaraaOthman <ao2000@hw.ac.uk>
2024-04-02 13:21:49 +0100

    updated

--- tree.py
+++ tree.py
@@ -4,11 +4,13 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
 #Following this tutorial https://www.datacamp.com/tutorial/decision-tree-classification-python
 
-headings = ['Searching for Item(s','Times add to basket was clicked','Time spent on individual product page','How many times add to basket was clicked from individual product page','How many times go to basket was clicked','How long spent on the checkout','Purchase Completed?']
-data = pd.read_csv("Dataset_Less_Time_on_Search_and_Checkout.csv", header=1, names=headings)
-features = ['Searching for Item(s','Times add to basket was clicked','Time spent on individual product page','How many times add to basket was clicked from individual product page','How many times go to basket was clicked','How long spent on the checkout']
+headings = ['Searching for Item(s','Times add to basket was clicked from search page','Time spent on individual product page','How many times add to basket was clicked from individual product page','How many times go to basket was clicked','How long spent on the checkout','Purchase Completed?']
+data = pd.read_csv("Dataset_Less_Time_on_Search.csv", header=1, names=headings)
+features = ['Searching for Item(s','Times add to basket was clicked from search page','Time spent on individual product page','How many times add to basket was clicked from individual product page','How many times go to basket was clicked','How long spent on the checkout']
 X = data[features]
 target = ['Purchase Completed?']
 y = data[target]
@@ -33,8 +35,9 @@ for i in range(len(importances)):
 #Pick important features
 
 
-imp_sorted = sorted(importance_match, key=importance_match.get, reverse=True)[:4]
-
+take = round(len(importance_match) * .7)
+imp_sorted = sorted(importance_match, key=importance_match.get, reverse=True)[:take]
+print(imp_sorted)
 values = []
 for i in range(len(imp_sorted)):
     values.append(importance_match[imp_sorted[i]])
@@ -49,6 +52,8 @@ ypred_best = decisionTree_best.predict(Xbesttest)
 
 print("Accuracy:",metrics.accuracy_score(y_test, ypred))
 print("Top 4 Accuracy",metrics.accuracy_score(y_test,ypred_best))
+confusion = confusion_matrix(y_test,ypred_best)
+print(confusion)
 fig = plt.figure(figsize=(5,5))
 
 plt.pie(importances, startangle=90,autopct='%1.1f%%',pctdistance=0.85,  textprops={'fontsize': 16})
